4bcf46f21f7b7fdca79212afb278821dfdd10fea
Markeev Andrew <mccorn13@gmail.com>
2024-03-05 08:38:44 +0300

    autofocus and autosave

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import { TodoData } from '../../types'
 import './Todo.css'
-import { useCallback, useState } from 'react';
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { patchTodo } from '../../store/reducers/TodosSlice';
 
@@ -15,22 +15,36 @@ function Todo({ data, onDelete, onDone }: TodoProps) {
   const [editMode, setEditMode] = useState(false);
   const [editValue, setEditValue] = useState(data.title);
   const dispatch = useDispatch();
+  const ref = useRef({}) as MutableRefObject<HTMLDivElement>;
+  const inputRef = useRef({}) as MutableRefObject<HTMLInputElement>;
 
-  const handleChangeEditValue =(value: string) => {
-    setEditValue(value)
-  }
-
-  const handleStartEdit = useCallback(() => {
-    setEditMode(true)
-  }, [])
+  const handleChangeEditValue = (value: string) => setEditValue(value);
 
   const handleEndEdit = useCallback(() => {
-    dispatch(patchTodo({id: data.id, title: editValue}))
-    setEditMode(false)
+    dispatch(patchTodo({ id: data.id, title: editValue }));
+    setEditMode(false);
   }, [data, editValue, dispatch])
 
+  const handleClickOutside = useCallback((event: Event) => {
+    const target = event.target as HTMLElement;
+    if (ref.current && !ref.current.contains(target)) {
+      dispatch(patchTodo(data));
+      setEditMode(false);
+      setEditValue(data.title);
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  }, [ref, data, dispatch])
+
+  const handleStartEdit = useCallback(() => {
+    setEditMode(true);
+    document.addEventListener("mousedown", handleClickOutside);
+  }, [handleClickOutside])
+
+  useEffect(() => () => document.removeEventListener("mousedown", handleClickOutside), [])
+  useEffect(() => {editMode ? inputRef.current.focus() : null}, [editMode])
+
   return (
-    <div id={`todo-${data.id}`} className={classNames('todoCard flex', { done: data.done })}>
+    <div id={`todo-${data.id}`} ref={ref} className={classNames('todoCard flex', { done: data.done })}>
       <div className='todoCard__body column'>
         <div className='todoCard__header flex'>
           <input className='todoCard__checkbox'
@@ -39,10 +53,13 @@ function Todo({ data, onDelete, onDone }: TodoProps) {
             onChange={onDone}
           />
           <div>
-            {editMode
-              ? <input value={editValue} onChange={(e) => handleChangeEditValue(e.target.value)}/>
-              : <p>{data.title}</p>
-            }
+            <input
+              className={classNames('todoCard__input', {hidden: !editMode})}
+              ref={inputRef}
+              value={editValue}
+              onChange={(e) => handleChangeEditValue(e.target.value)}
+            />
+            {!editMode && <p>{data.title}</p>}
 
           </div>
         </div>
